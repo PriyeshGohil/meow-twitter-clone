078fe5da61500faac2235f13b05743f64b8fe1a0
Priyesh Gohil <priyesh114@hotmail.co.uk>
2016-03-25 14:52:28 +0000

    replaced hbs module to express-handlebars

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var exphbs  = require('express-handlebars');
 
 var app = express();
 
-app.set('view engine', 'hbs');
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.use(express.static('public'));
 
 app.get('/', function(req, res) {
