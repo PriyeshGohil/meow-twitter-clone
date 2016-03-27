7265be6a86dd7a173e96ef772af0fc6d70da8ce2
Priyesh Gohil <priyesh114@hotmail.co.uk>
2016-03-28 00:07:44 +0100

    custom method to hash password whening creating a new user

--- models/users.js
+++ models/users.js
@@ -1,8 +1,24 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
+var bcrypt = require('bcryptjs');
 
 var userSchema = new Schema({
   username = {type: String, required: true},
   email = {type: String, required: true},
   password = {type: String, required: true},
 });
+
+module.exports = mongoose.model('User', userSchema);
+
+// custom method for creating a new user
+userSchema.pre('save', function(next) {
+  // if password is already hashed move on.
+  if(this.password) next();
+
+  bcrypt.genSalt(10, function(err,salt) {
+    bcrypt.hash(this.password, salt, function(err,hash) {
+        this.password = hash;
+    });
+  });
+
+})
